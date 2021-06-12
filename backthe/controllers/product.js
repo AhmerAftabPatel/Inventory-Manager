545e2f3558a3d2f097487352eb5c5cd8524a2d56
AhmerAftabPatel <69456880+AhmerAftabPatel@users.noreply.github.com>
2021-06-12 16:41:13 +0530

    Update product.js

--- backthe/controllers/product.js
+++ backthe/controllers/product.js
@@ -22,32 +22,7 @@ function checkFileType(file, cb) {
     cb("Error: Images Only!");
   }
 }
-const s3 = new aws.S3({
-  accessKeyId: "AKIAJX3NISA5AKBGRFHQ",
-  secretAccessKey: "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",
-  Bucket: "btobsales"
-});
 
-const uploadsBusinessGallery = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: "btobsales",
-    acl: "public-read",
-    key: function (req, file, cb) {
-      cb(
-        null,
-        path.basename(file.originalname, path.extname(file.originalname)) +
-          "-" +
-          Date.now() +
-          path.extname(file.originalname)
-      );
-    }
-  }),
-  limits: { fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).array("stock_images", 4);
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
